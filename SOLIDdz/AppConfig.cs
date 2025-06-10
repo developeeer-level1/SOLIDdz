d2e11f02259e7646aeca3e38d7b515a77bcbec3a
developeeer-level1 <yak.yakovochka@gmail.com>
2025-06-10 22:43:26 +0300

    Add project files.

---
+++ SOLIDdz/AppConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+
+public class AppConfig
+{
+    private static AppConfig instance;
+    private static object @lock = new object();
+    static Dictionary<string, string> settings;
+    private static string filePath = "config.json";
+    private AppConfig()
+    {
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            settings = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+        }
+        else
+        {
+            settings = new Dictionary<string, string>();
+        }
+    }
+    public static void SetSetting(string key, string value)
+    {
+        settings[key] = value;
+        SaveToFile();
+    }
+    public static string GetSetting(string key)
+    {
+        return settings.ContainsKey(key) ? settings[key] : null;
+    }
+    public static void SaveToFile()
+    {
+        string json = JsonSerializer.Serialize(settings, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(filePath, json);
+    }
+}
